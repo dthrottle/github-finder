e7e1820ac88cbdb506317d66d1255d6a3c7a38e0
Ted <tdonald@gmail.com>
2023-05-26 22:26:15 +0300

    about page and 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer';
 
@@ -12,7 +15,12 @@ function App() {
         <Navbar />
 
         <main className='container mx-auto px-3 pb-12'>
-          Content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound/>} />
+          </Routes>
         </main>
         <Footer />
       </div>
