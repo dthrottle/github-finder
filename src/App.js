cbb99e0861c205358c06dff9b2a24d1c9df02975
Ted <tdonald@gmail.com>
2023-05-26 05:55:11 +0300

    add navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>Hello World</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main className='container mx-auto px-3 pb-12'>
+          Content
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
